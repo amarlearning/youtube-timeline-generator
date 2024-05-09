99a560dbaefa4fb86a6d0acf8e0980204dd4f58d
Shruti Dhavalikar <shrutid@gmail.com>
2024-05-09 20:06:12 +0530

    Tighten the prompt for aggregation

--- main.py
+++ main.py
@@ -19,11 +19,10 @@ def main():
         model_name=model_name, system_prompt=system_prompt, api_endpoint=api_endpoint)
 
     system_prompt_aggregation = '''
-       Aggregate the below time intervals into only 5 to 10 buckets over the entire time period. 
-       Each interval should contain a short summary(in a single phrase) of topic discussed in the conversation.
-       Create a new bucket when there is a change in the topic.
-       Do not exceed the timestamps given by the user while bucketing.
-            Example, 
+       For a given video a list of various time buckets with a short summary will be provided by a user.
+       Please group the time buckets into not more than 5 to 10 time buckets. Group them over the similarity of the been topics discussed.
+       Do not exceed the timestamps given by the user while grouping. Consider the entire time period.
+            Example of output- 
             00:01:06 - 00:01:35 = Introduction 
             00:01:35 - 00:02:47 = Overview of civil engineering
         '''
