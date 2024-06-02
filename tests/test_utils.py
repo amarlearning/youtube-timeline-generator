70aa2f0a89c2d997f00c98db7a862532779ea46a
Amar Prakash Pandey <amar.om1994@gmail.com>
2024-06-02 23:32:11 +0530

    feat: add test for utils files

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,22 @@
-from llm_video_timeline_description.utils import process_srt_content
+from llm_video_timeline_description.utils import process_srt_content, clean_srt_content, split_cleaned_content, clean_aggregated_timeline
 
 
 def test_process_data():
-    data = "sample data"
+    data = """
+        1
+        00:00:01,000 --> 00:00:04,000
+        This is the first subtitle.
+
+        2
+        00:00:05,000 --> 00:00:09,000
+        This is the second subtitle.
+
+        3
+        00:00:10,000 --> 00:00:14,000
+        This is the third subtitle.
+    """
+
     result = process_srt_content(data)
-    expected_result = ["sample data "]
+    expected_result = [
+        "\n\n00:00:01  This is the first subtitle. \n\n00:00:05  This is the second subtitle. \n\n00:00:10  This is the third subtitle. \n"]
     assert result == expected_result, f'Expected {expected_result}, got {result}'
