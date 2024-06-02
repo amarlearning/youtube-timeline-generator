eec598dd8a9d17ade56bce108557b08cf32fd0e9
Amar Prakash Pandey <amar.om1994@gmail.com>
2024-06-02 14:33:14 +0530

    feat: Add response object and UI feats

--- llm_video_timeline_description/main.py
+++ llm_video_timeline_description/main.py
@@ -9,7 +9,8 @@ from llm_video_timeline_description.video_srt import get_srt_from_youtube_video
 
 app = Flask(__name__)
 time_bucketing_agent = UndercoverLLMAgent(system_prompt=SYSTEM_QUERY_PROMPT)
-time_bucketing_aggregation_agent = UndercoverLLMAgent(system_prompt=SYSTEM_AGGREGATION_PROMPT)
+time_bucketing_aggregation_agent = UndercoverLLMAgent(
+    system_prompt=SYSTEM_AGGREGATION_PROMPT)
 summary_agent = UndercoverLLMAgent(system_prompt=SUMMARY_PROMPT)
 
 
@@ -21,23 +22,30 @@ def home():
 @app.route('/submit', methods=['POST'])
 def submit_video():
     video_url = request.form['video_url']
+
     srt_content = get_srt_from_youtube_video(video_url)
+
     split_description_content = split_and_clean_srt_content(srt_content)
-    time_buckets = get_time_bucketing(split_description_content, time_bucketing_agent)
+
+    time_buckets = get_time_bucketing(
+        split_description_content, time_bucketing_agent)
     print(f"time_buckets: {time_buckets}")
+
     aggregated_time_buckets = time_bucketing_aggregation_agent.find_answer_for(
         Message(role="user", content=time_buckets)
     ).content
-    print(f"aggregated_time_buckets: {aggregated_time_buckets}")
 
     processed_response = postprocessing(aggregated_time_buckets)
-    print(f"processed_response: {processed_response}")
 
-    # summary = summary_agent.find_answer_for(
-    #     Message(role="user", content=time_buckets)
-    # ).content
-    # print(f"summary: {summary}")
-    return "yo", 200
+    summary = summary_agent.find_answer_for(
+        Message(role="user", content=time_buckets)
+    ).content
+
+    response = {
+        "summary": summary,
+        "timeline": processed_response
+    }
+    return response, 200
 
 
 if __name__ == "__main__":
--- llm_video_timeline_description/static/script.js
+++ llm_video_timeline_description/static/script.js
@@ -2,22 +2,46 @@ $(document).ready(function () {
   $("form").on("submit", function (event) {
     event.preventDefault();
     var videoUrl = $("#video_url").val();
-    $("#spinner").css("display", "block");
-    $("button[type='submit']").prop("disabled", true);
+
+    showLoadingSpinner();
+
     $.post("/submit", { video_url: videoUrl }, function (data) {
-      $("#spinner").css("display", "none");
-      $("button[type='submit']").prop("disabled", false);
+      hideLoadingSpinner();
       if (data) {
-        $("#data").css("padding", "10px");
+        $("#response").css("display", "block");
+        $("#summary-content").html(data.summary);
+        $("#timeline-content").html(data.timeline.replace(/\n/g, "<br>"));
       }
-      $("#data").html(JSON.stringify(data).replace(/^"|"$/g, ""));
     })
       .fail(function () {
         alert("An error occurred. Please try again.");
       })
       .always(function () {
-        $("#spinner").css("display", "none");
-        $("button[type='submit']").prop("disabled", false);
+        hideLoadingSpinner();
       });
   });
+});
+
+function showLoadingSpinner() {
+  $("#spinner").css("display", "block");
+  $("button[type='submit']").prop("disabled", true);
+}
+
+function hideLoadingSpinner() {
+  $("#spinner").css("display", "none");
+  $("button[type='submit']").prop("disabled", false);
+}
+
+$(".copy-icon").on("click", function () {
+  var copyTarget = $(this).data("copy-target");
+  var textToCopy = $(copyTarget).html();
+
+  navigator.clipboard
+    .writeText(textToCopy)
+    .then(function () {
+      $("#copy-toast").toast("show");
+    })
+    .catch(function () {
+      console.log("Failed to copy text");
+    });
 });
